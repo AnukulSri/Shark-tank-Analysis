b0c6017d6c1807d6235a919bd4198039f05ac44c
Anukul Srivastava <anukulsrivastava123@gmail.com>
2023-09-21 11:19:02 +0530

    Create Data Query.sql

---
+++ Data Query.sql
@@ -0,0 +1,40 @@
+Create database Shark_tank
+
+use shark_tank
+
+Create table content(
+Ep_no int not null,
+Brand varchar(50),
+Male int,
+Female int,
+Location varchar(20),
+Idea varchar(120),
+Sector varchar(50),
+deal varchar(80),
+Amount_invested_in_lakhs int,
+Amount_asked int,
+debt_invested int,
+debt_asked int,
+equity_taken float,
+equity_asked float,
+Average_age int,
+Team_member int,
+Ashneer_invested_amount float,
+Ashneer_equity_taken float,
+Namita_invested_amount int,
+Namita_equity_taken float,
+Anupam_invested_amount float,
+Anupam_equity_taken float,
+Vinita_invested_amount float,
+Vinita_equity_taken float,
+Aman_invested_amount float,
+Aman_equity_taken float,
+Piyush_invested_amount float,
+Piyush_equity_taken float,
+Ghazal_invested_amount float,
+Ghazal_equity_taken float,
+Total_investor int,
+Partners varchar(50)
+
+)
+
